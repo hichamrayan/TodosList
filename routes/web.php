83ab5ebf21964d03f1f2b0526f01247be5a9e222
hichamrayan <hichamm.bouzkraoui@gmail.com>
2020-06-30 18:04:25 -0600

    Restored Project from Linux

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+// Route::middleware('auth')->group(function () {
+    Route::resource('/todo', 'TodoController');
+    Route::put('/todos/{todo}/complete', 'TodoController@complete')->name('todo.complete');
+    Route::delete('/todos/{todo}/incomplete', 'TodoController@incomplete')->name('todo.incomplete');
+// });
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/user', 'UserController@index');
+
+Route::post('/upload', 'UserController@uploadAvatar');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
